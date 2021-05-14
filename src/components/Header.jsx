bbb09f065c87ed60b475238089843f6c0662cab2
heorhi-bel <georgix@mail.ru>
2021-05-14 16:11:43 +0200

    some changes with refferences + connect with github pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,18 @@ function Header() {
     return (
         <nav className="pink lighten-3">
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo">
+                <a href="/" className="brand-logo">
                     React Shop
                 </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li>
-                        <a href="sass.html">Repo</a>
+                        <a
+                            href="https://github.com/heorhi-bel/shop_react"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Repo
+                        </a>
                     </li>
                     <li>
                         <a href="badges.html">Else</a>
